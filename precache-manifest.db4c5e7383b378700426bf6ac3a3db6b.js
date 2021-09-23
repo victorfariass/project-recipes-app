9f9d0e1a5d1d938890145c93581ec115f08fb145
victor Farias <victorfarias.tv@gmail.com>
2021-09-23 11:34:14 -0300

    Updates

---
+++ precache-manifest.db4c5e7383b378700426bf6ac3a3db6b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb149eb3518314871e7a6aa7a84a63c",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "3ac1ff4d7b0c32260bc0",
+    "url": "/recipes/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "4ffe44423586bfe9c145",
+    "url": "/recipes/static/css/main.4a58f59e.chunk.css"
+  },
+  {
+    "revision": "3ac1ff4d7b0c32260bc0",
+    "url": "/recipes/static/js/2.800a81b5.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/recipes/static/js/2.800a81b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffe44423586bfe9c145",
+    "url": "/recipes/static/js/main.c66b51ab.chunk.js"
+  },
+  {
+    "revision": "7ce9a55ad9942d96b3ba",
+    "url": "/recipes/static/js/runtime-main.9b0c4d8d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72a54cc6f1bdf4b3915f55c75a89c6d1.js"
+  "/recipes/precache-manifest.db4c5e7383b378700426bf6ac3a3db6b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
